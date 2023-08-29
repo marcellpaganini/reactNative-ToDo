e7c934fd6dba37950f1dc4b60905a724a72a9c60
paganini.marcell <paganini.marcell@gmail.com>
2023-08-29 09:49:00 -0300

    Header and hardcoded list added

--- Todo/App.tsx
+++ Todo/App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text: 'Study', key: '1'},
+    {text: 'Work on project', key: '2'},
+    {text: 'Wash the dog', key: '3'},
+  ])
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
+      <View style={styles.content}>
+        {/*To Form*/}
+        <View style={styles.list}>
+          <FlatList 
+            data={todos} 
+            renderItem={({ item }) => (
+              <Text>{ item.text }</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +29,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
+  }
 });
+
+
